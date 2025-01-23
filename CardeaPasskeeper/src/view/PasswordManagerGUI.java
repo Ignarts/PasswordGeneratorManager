5f5d0444fca3d0e38973416f775e77a8af873643
Ignacio Meléndez <ignaciomelendezuriz@gmail.com>
2025-01-23 12:58:24 +0100

    Add colors toto app and create TextsFields

--- CardeaPasskeeper/src/view/PasswordManagerGUI.java
+++ CardeaPasskeeper/src/view/PasswordManagerGUI.java
@@ -12,9 +12,17 @@ import java.awt.event.ActionListener;
 public class PasswordManagerGUI {
     public static PasswordController controller = new PasswordController();
 
+    private static final Color brown = new Color(101, 77, 56);
+    private static final Color lightBrown = new Color(241, 189, 143);
+    private static final Color lightGreen = new Color(201, 190, 127);
+    private static final Color green = new Color(128, 162, 100);
+    private static final Color darkGreen = new Color(87, 116, 77);
+
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Password Manager");
+        JFrame frame = new JFrame("Cardea Passkeeper");
         frame.setLayout(new FlowLayout());
+
+        createTextsFields(frame);
 
         createAddPasswordButton(frame);
         createRemovePasswordButton(frame);
@@ -22,12 +30,16 @@ public class PasswordManagerGUI {
         frame.setSize(400, 300);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.getContentPane().setBackground(lightBrown);
+        frame.setResizable(false);
     }
 
     // Create Add Password Button and add logic to it
     public static void createAddPasswordButton(JFrame frame) {
         JButton addPasswordButton = new JButton("Add Password");
         frame.add(addPasswordButton);
+        addPasswordButton.setBackground(green);
 
         addPasswordButton.addActionListener(new ActionListener() {
             @Override
@@ -42,6 +54,7 @@ public class PasswordManagerGUI {
     public static void createRemovePasswordButton(JFrame frame) {
         JButton removePasswordButton = new JButton("Remove Password");
         frame.add(removePasswordButton);
+        removePasswordButton.setBackground(darkGreen);
 
         removePasswordButton.addActionListener(new ActionListener() {
             @Override
@@ -50,5 +63,16 @@ public class PasswordManagerGUI {
                 controller.removePassword(entry);
             }
         });
+    }
+
+    public static void createTextsFields(JFrame frame) {
+        JTextField applicationName = new JTextField("");
+        applicationName.setPreferredSize(new Dimension(200, 30));
+
+        JTextField username = new JTextField("");
+        username.setPreferredSize(new Dimension(200, 30));
+
+        frame.add(applicationName);
+        frame.add(username);
     }
 }
